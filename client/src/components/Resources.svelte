de9b087e642e437a0206dcec3244417e44051b0d
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-04 15:31:54 +0300

    Update/ image paths in Footer and Home components

--- client/src/components/Resources.svelte
+++ client/src/components/Resources.svelte
@@ -1,17 +1,17 @@
 <section id="resources" class="bg-black">
     <div class="max-w-[1240px] mx-auto py-[4rem] px-5">
         <div class="text-center max-w-[850px] mx-auto ">
-            <h1 class="text-3xl font-bold">Resources</h1>
-            <p class="my-5 text-2xl text-gray-400">SomAtlasAPI offers two key resources designed to enhance your application development:</p>
+            <h1 class="text-3xl font-bold">API Resources</h1>
+            <p class="pt-5 text-lg text-gray-400">SomAtlasAPI offers two key resources designed to enhance your application development:</p>
         </div>
         
         <div class="my-20">
-           <p class="lg:text-2xl md:text-xl bg-green-300 inline-block p-0.5 rounded-sm text-black"><a href="https://www.api.somatlas.dev/api/v1/states">api/v1/states</a></p>
-         <p class="py-4 lg:text-2xl md:text-xl">This endpoint provides a comprehensive list of states and districts within Somalia. It facilitates the retrieval of detailed information about each region.</p>
+           <p class="lg:text-xl md:text-xl bg-green-300 inline-block p-0.5 rounded-sm text-black"><a href="https://www.api.somatlas.dev/api/v1/states">api/v1/states</a></p>
+         <p class="py-4 lg:text-base md:text-xl">This endpoint provides a comprehensive list of states and districts within Somalia. It facilitates the retrieval of detailed information about each region.</p>
         </div>
         <div class="my-10">
-           <p class="lg:text-2xl md:text-xl bg-green-300 inline-block p-0.5 rounded-sm text-black"><a href="https://www.api.somatlas.dev/api/v1/states/1">api/v1/states/1</a></p>
-         <p class="py-4 lg:text-2xl md:text-xl">This endpoint allows the retrieval of detailed information about a specific state in Somalia. The :id parameter represents the unique identifier of the state.</p>
+           <p class="lg:text-xl md:text-xl bg-green-300 inline-block p-0.5 rounded-sm text-black"><a href="https://www.api.somatlas.dev/api/v1/states/1">api/v1/states/1</a></p>
+         <p class="py-4 lg:text-base md:text-xl">This endpoint allows the retrieval of detailed information about a specific state in Somalia. The :id parameter represents the unique identifier of the state.</p>
         </div>
       
     </div>

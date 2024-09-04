bbccb8b1af44b195ea2f85cce5fe17fecd1d2d92
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-04 15:32:54 +0300

    Update Features component with improved text styling

--- client/src/components/Features.svelte
+++ client/src/components/Features.svelte
@@ -5,7 +5,7 @@
     <div class="max-w-[1240px]  mx-auto py-[4rem] px-5">
    <div class="max-w-[850px] mx-auto">
     <h1 class="text-center text-3xl font-bold text-black">Why Choose <span class="text-green-400">SomAtlasAPI?</span></h1>
-    <p class="mt-10 text-2xl text-center  text-gray-600">SomAtlasAPI is your all-encompassing platform, providing effortless access to explore detailed information on states and districts in Somalia through our seamless API.</p>
+    <p class="pt-5 text-lg text-center  text-gray-600">SomAtlasAPI is your all-encompassing platform, providing effortless access to explore detailed information on states and districts in Somalia through our seamless API.</p>
    </div>
     <div class="grid gap-4 md:grid-cols-3 sm:grid-cols-1 py-[4rem]">
         <div class="w-full  shadow-2xl bg-black mt-10 py-4 items-center text-center text-white flex flex-col rounded-lg hover:scale-105 duration-300">
@@ -14,7 +14,7 @@
 
             <div class="font-medium text-center">
                 <h1 class="pt-5 text-xl font-bold mx-4">Comprehensive Access</h1>
-                <p class="py-8  text-lg mx-4">Instantly access detailed data on Somalia's states and districts.</p>
+                <p class="py-8  text-base mx-4">Instantly access detailed data on Somalia's states and districts.</p>
             </div>
         </div>
         <div class="w-full shadow-xl bg-green-300 mt-10 py-4  items-center text-center text-black  flex flex-col rounded-lg hover:scale-105 duration-300 ">
@@ -23,7 +23,7 @@
 
             <div class="font-medium text-center">
                 <h1 class="pt-5 text-xl font-bold mx-4">Simplified Registration</h1>
-                <p class="py-8  text-lg mx-4">Effortlessly integrate location data into your registration process.</p>
+                <p class="py-8  text-base mx-4">Effortlessly integrate location data into your registration process.</p>
             </div>
         </div>
         <div class="w-full shadow-xl bg-black mt-10 py-4 items-center text-center text-white  flex flex-col rounded-lg hover:scale-105 duration-300">
@@ -32,7 +32,7 @@
 
             <div class="font-medium text-center">
                 <h1 class="pt-5 text-xl font-bold mx-4"> Real-Time Updates</h1>
-                <p class="py-8  text-lg mx-4">Keep your app current with dynamic geographic information..</p>
+                <p class="py-8  text-base mx-4">Keep your app current with dynamic geographic information..</p>
             </div>
         </div>
        
